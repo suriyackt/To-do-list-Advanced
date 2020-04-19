f3d882d689e85438692cac6a4561bfda6903600e
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-04-19 18:30:49 +0530

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = ()=>{
+    const date = new Date();
+
+    const year = date.getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright &copy; {year}</p>
+        </footer>
+    );
+};
+
+export default Footer;
+
+
